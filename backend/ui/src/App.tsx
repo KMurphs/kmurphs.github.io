b3d812e5c8cc9497bbb89418f00e3039aedc3d35
stephane kibonge <stephane.kibonge@etion.co.za>
2019-11-18 15:27:12 +0200

    Add AboutMe blocks

--- backend/ui/src/App.tsx
+++ backend/ui/src/App.tsx
@@ -9,8 +9,8 @@ const App: React.FC = () => {
     <div className="App">
       <Nav/>
       <div className="Pages">
-        <Home/>
-        {/* <AboutMe/> */}
+        {/* <Home/> */}
+        <AboutMe/>
       </div>
     </div>
   );
--- backend/ui/src/components/AboutMe/AboutMe.tsx
+++ backend/ui/src/components/AboutMe/AboutMe.tsx
@@ -8,15 +8,76 @@ const AboutMe: React.FC = () => {
         <span><h3>About Me</h3></span>
         <span><p>Artist, Thinker, Creative Doer</p></span>
       </div>
+
       <div className="presentation">
-        <div><h1>1</h1></div>
-        <div><h1>2</h1></div>
-        <div><h1>3</h1></div>
+        <div>
+          <span><p>Presentation</p></span>
+        </div>
+        <div className="row">
+          <div className="col-sm-12 col-lg-6 col-xl-4"><h1>1</h1></div>
+          <div className="col-sm-12 col-lg-6 col-xl-4"><h1>2</h1></div>
+          <div className="col-sm-12 col-lg-6 col-xl-4"><h1>3</h1></div>
+        </div>
       </div>
-      <div className="services"></div>
-      <div className="clients"></div>
-      <div className="testimonials"></div>
-      <div className="fun-facts"></div>
+
+      <div className="services">
+        <div>
+          <span><p>services</p></span>
+        </div>
+        <div className="row">
+          <div className="col-sm-12 col-lg-6 col-xl-3"><h1>1</h1></div>
+          <div className="col-sm-12 col-lg-6 col-xl-3"><h1>2</h1></div>
+          <div className="col-sm-12 col-lg-6 col-xl-3"><h1>3</h1></div>
+          <div className="col-sm-12 col-lg-6 col-xl-3"><h1>3</h1></div>
+        </div>
+      </div>
+
+
+
+      <div className="clients">
+        <div>
+          <span><p>clients</p></span>
+        </div>
+        <div className="row">
+            <div className="col-sm-12 col-md-4 col-xl-2"><h1>1</h1></div>
+            <div className="col-sm-12 col-md-4 col-xl-2"><h1>2</h1></div>
+            <div className="col-sm-12 col-md-4 col-xl-2"><h1>3</h1></div>
+            <div className="col-sm-12 col-md-4 col-xl-2"><h1>1</h1></div>
+            <div className="col-sm-12 col-md-4 col-xl-2"><h1>2</h1></div>
+            <div className="col-sm-12 col-md-4 col-xl-2"><h1>3</h1></div> 
+        </div>
+      </div>
+
+
+      <div className="testimonials">
+        <div>
+          <span><p>testimonials</p></span>
+        </div>
+        <div className="row">
+            <div className="col-sm-12 col-md-6 col-xl-4"><h1>1</h1></div>
+            <div className="col-sm-12 col-md-6 col-xl-4"><h1>2</h1></div>
+            <div className="col-sm-12 col-md-6 col-xl-4"><h1>3</h1></div>
+        </div>
+      </div>
+
+
+      <div className="fun-facts">
+        <div>
+          <span><p>fun-facts</p></span>
+        </div>
+        <div className="row">
+          <div className="col-sm-12 col-lg-6 col-xl-3"><h1>1</h1></div>
+          <div className="col-sm-12 col-lg-6 col-xl-3"><h1>2</h1></div>
+          <div className="col-sm-12 col-lg-6 col-xl-3"><h1>3</h1></div>
+          <div className="col-sm-12 col-lg-6 col-xl-3"><h1>3</h1></div>
+        </div>
+      </div>
+
+
+
+
+
+
     </div>
   );
 }
