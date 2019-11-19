ec2dea178981c87417acd900db4807d76eabd99d
stephane kibonge <stephane.kibonge@etion.co.za>
2019-11-19 11:39:28 +0200

    Complete components transition

--- backend/ui/src/components/Home/Home.tsx
+++ backend/ui/src/components/Home/Home.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
-// import './Home.css';
+import React, { useState } from 'react';
+import useTransition from '../TransitionHook/useTransition';
+import './Home.css';
+
+
 type Props = {
   isCurrentlyDisplayed: boolean
 }
+
 const Home: React.FC<Props> = (props) => {
+  
+  const [hasDefaultState, setHasDefaultState] = useState<boolean>(true)
+  const {transition} = useTransition(props.isCurrentlyDisplayed, ()=>setHasDefaultState(true), ()=>setHasDefaultState(false))
+  
   return (
-    <div className="home">
+    <div className={`home ${props.isCurrentlyDisplayed?'':'home--invisible'} ${hasDefaultState?'home--default':''}`}>
       <h1>Alex Smith</h1>
       <p>Web Developper</p>
     </div>
