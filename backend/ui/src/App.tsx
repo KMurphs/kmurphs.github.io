7bee6188bd646b8cf907b7c07e9e649fe4519081
Stephan K <KibongeSP@gmail.com>
2020-01-13 23:42:48 +0200

    Complete Hero

--- backend/ui/src/App.tsx
+++ backend/ui/src/App.tsx
@@ -14,7 +14,7 @@ const App: React.FC = () => {
         <div className="flex-1 flex">
           <div className={`pt-16 flex-none h-full w-40 bg-gray-200 transition-600 ${!isPaneOpen?'-ml-40':'-ml-0'}`}>
             Hello from side
-            <Menu menuState={isPaneOpen} onMenuStateChange={setIsPaneOpen} extraClasses={'fixed left-p2 bottom-p2 rotate-p270-scale-800'}/>
+            <Menu menuState={isPaneOpen} onMenuStateChange={setIsPaneOpen} extraClasses={'hidden sm:block fixed left-p2 bottom-p2 rotate-p270-scale-800'}/>
           </div>
           <div className="flex-1 bg-gray-100 transition-600"><Hero /></div>
         </div>   
--- backend/ui/src/components/Hero/Hero.tsx
+++ backend/ui/src/components/Hero/Hero.tsx
@@ -7,18 +7,18 @@ const Hero: React.FC = () => {
 
     return (
 
-        <section className="hero h-full bg-anchored flex justify-between items-center" style={{backgroundImage: 'url(/imgs/hero-bg.jpg)'}}>
-            <div className="p-8 ml-8 text-gray-100 ">
+        <section className="hero p-8 h-full bg-anchored flex justify-center items-center flex-col lg:flex-row lg:items-center lg:justify-between" style={{backgroundImage: 'url(/imgs/hero-bg.jpg)'}}>
+            <div className="hero-text mx-8 text-gray-100 my-8">
                 <h1 className="text-4xl py-8">Lorem ipsum dolor sit amet.</h1>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio, alias debitis maxime aspernatur quasi officia iste! Eligendi corporis vitae impedit sapiente necessitatibus, voluptates vel facilis, aut voluptas itaque quasi praesentium harum dolore eos esse perferendis. Incidunt ad suscipit commodi nesciunt, perspiciatis illo inventore ipsa ut impedit, a tempore quis sequi.</p>
+                <p className="text-justify hyphen-auto">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio, alias debitis maxime aspernatur quasi officia iste! Eligendi corporis vitae impedit sapiente necessitatibus, voluptates vel facilis, aut voluptas itaque quasi praesentium harum dolore eos esse perferendis. Incidunt ad suscipit commodi nesciunt, perspiciatis illo inventore ipsa ut impedit, a tempore quis sequi.</p>
             </div>
-            <div className="cta ml-8 flex flex-center  p-8 text-gray-100">
+            <div className="cta mx-8 mt-8 flex flex-center text-gray-100">
                 <div className="p-8 border border-gray-100 bg-mask-dark-500 rounded-lg ">
                     <h2 className="text-2xl pb-8">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum, sequi.</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid mollitia totam exercitationem culpa, optio quibusdam ratione dicta sed architecto libero! </p>
-                    <div className="flex justify-between mt-8">
-                        <a href="/" className="w-48 py-2 flex flex-center border-2 border-blue-500 px-8 rounded-lg text-white inline-block hover:bg-blue-900 transition-300 bg-blue-500"><span>View Projects</span></a>
-                        <a href="/" className="w-48 py-2 flex flex-center border-2 border-blue-500 px-8 rounded-lg text-white inline-block hover:bg-blue-900 transition-300 bg-transparent"><span>Contact Me</span></a>
+                    <div className="flex items-stretch flex-col sm:flex-row sm:justify-between mt-4">
+                        <a href="/" className="mt-4 py-2 flex flex-center border-2 border-blue-500 px-4 rounded-lg text-white inline-block hover:bg-blue-900 transition-300 bg-blue-500"><span>View Projects</span></a>
+                        <a href="/" className="mt-4 py-2 flex flex-center border-2 border-blue-500 px-4 rounded-lg text-white inline-block hover:bg-blue-900 transition-300 bg-transparent"><span>Contact Me</span></a>
                     </div>
 
                 </div>
